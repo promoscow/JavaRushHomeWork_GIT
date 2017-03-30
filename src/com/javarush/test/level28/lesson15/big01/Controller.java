4233a71ce3ffbe5149d8acec53cb2f125bbda2b7
promoscow <slava_rossii@list.ru>
2017-03-30 23:47:47 +0300

    парсер 3 сдано

--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -2,20 +2,23 @@ package com.javarush.test.level28.lesson15.big01;
 
 import com.javarush.test.level28.lesson15.big01.model.Provider;
 
+import java.util.Arrays;
+
 /**
  * Created by promoscow on 23.02.17.
  */
 public class Controller {
-    private Provider providers;
+    private Provider[] providers;
 
-    public Controller(Provider providers) throws IllegalArgumentException {
+    public Controller(Provider... providers) throws IllegalArgumentException {
+        if (providers == null || providers.length == 0) throw new IllegalArgumentException();
         this.providers = providers;
     }
 
     @Override
     public String toString() {
         return "Controller{" +
-                "providers=" + providers +
+                "providers=" + Arrays.toString(providers) +
                 '}';
     }
 }
---
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level28.lesson15.big01.model;
+
+/**
+ * Created by promoscow on 30.03.17.
+ */
+public class HHStrategy implements Strategy {
+}
