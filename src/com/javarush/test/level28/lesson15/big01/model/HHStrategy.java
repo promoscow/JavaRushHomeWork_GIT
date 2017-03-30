74fb28371ecfeba7c3f99ac8eeeff90c71339bec
promoscow <slava_rossii@list.ru>
2017-03-31 00:08:19 +0300

    парсер 4 сдано

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -4,4 +4,5 @@ package com.javarush.test.level28.lesson15.big01.model;
  * Created by promoscow on 30.03.17.
  */
 public class HHStrategy implements Strategy {
+    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
 }
